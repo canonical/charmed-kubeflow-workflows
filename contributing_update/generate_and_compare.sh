95e1c0fb8637072fdb1114564dfabfff03d8eb58
ColmBhandal <colm.bhandal@canonical.com>
2023-07-19 17:21:52 +0100

    Generate from template
    Read in the TOMLs to bash env vars
    Then loop through all placeholders and replace them
    
    Write to the contributing.md file in the temp dir
    Print out file contents at the end

--- contributing_update/generate_and_compare.sh
+++ contributing_update/generate_and_compare.sh
@@ -9,18 +9,22 @@ TEMPLATE_FILE="$DIR_PATH/contributing.md.template"
 # Define the path to the inputs file relative to the current working directory
 INPUTS_FILE="contributing_inputs.toml"
 
-# Read and display the contents of the template file
-if [[ -f "$TEMPLATE_FILE" ]]; then
-    echo "Template file contents:"
-    cat "$TEMPLATE_FILE"
-else
-    echo "Template file does not exist: $TEMPLATE_FILE"
-fi
+# Create a new file called contributing.md in the specified directory
+OUTPUT_FILE="$DIR_PATH/contributing.md"
 
-# Read and display the contents of the inputs file if it exists in the current working directory
-if [[ -f "$INPUTS_FILE" ]]; then
-    echo "Inputs file contents:"
-    cat "$INPUTS_FILE"
-else
-    echo "Inputs file does not exist: $INPUTS_FILE"
-fi
+# Read the values from the inputs file (TOML)
+source <(tomlq -r "$INPUTS_FILE")
+
+# Read the template file contents
+template=$(cat "$TEMPLATE_FILE")
+
+# Replace placeholders in the template with corresponding values from TOML
+for key in "${!template[@]}"; do
+  template=${template//\{\{$key\}\}/${!key}}
+done
+
+# Write the modified template to the output file
+echo "$template" > "$OUTPUT_FILE"
+
+echo "Generated contributing file from template. Contents: \n\n"
+cat "$OUTPUT_FILE"
