ca70d2113a609be17c142ec1a9e13949fe6b1c2e
ColmBhandal <colm.bhandal@canonical.com>
2023-08-03 10:44:54 +0100

    Use set-output

--- contributing_update/generate_and_compare.sh
+++ contributing_update/generate_and_compare.sh
@@ -71,8 +71,8 @@ fi
 # Compare the substituted template with the existing contributing contents
 if [ "$template" = "$existingContributingContents" ]; then
   echo "Contributing file is up to date. No need for a PR."
-  echo "comparison_result=0" >> $GITHUB_ENV
+  echo "::set-output name=comparison_result::0"
 else
   echo "Contributing file does not exist or is outdated - a PR is needed."
-  echo "comparison_result=1" >> $GITHUB_ENV
+  echo "::set-output name=comparison_result::1"
 fi
